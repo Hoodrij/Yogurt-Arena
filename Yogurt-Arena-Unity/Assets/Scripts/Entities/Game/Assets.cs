6db680db38def1d0b2eabff24f7ce156340e2e17
Slava Korzun <Hoodrij@gmail.com>
2023-03-08 10:54:27 +0200

    Prefabs movement

--- Yogurt-Arena-Unity/Assets/Scripts/Entities/Game/Assets.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Game/Assets.cs
@@ -4,11 +4,13 @@ namespace Yogurt.Arena
 {
     public class Assets : IComponent
     {
+        public const string PREFABS_PATH = "Prefabs/";
+        
         public readonly SO<Data> Data = new("Data"); 
         
-        public readonly Asset<World> World = new Asset<World>("World");
-        public readonly Asset<CameraView> Camera = new Asset<CameraView>("Camera");
-        public readonly Asset<InputFieldView> InputField = new Asset<InputFieldView>("InputField");
-        public readonly Asset<BeaconView> Beacon = new Asset<BeaconView>("Beacon");
+        public readonly Asset<World> World = new Asset<World>(PREFABS_PATH + "World");
+        public readonly Asset<CameraView> Camera = new Asset<CameraView>(PREFABS_PATH + "Camera");
+        public readonly Asset<InputFieldView> InputField = new Asset<InputFieldView>(PREFABS_PATH + "InputField");
+        public readonly Asset<BeaconView> Beacon = new Asset<BeaconView>(PREFABS_PATH + "Beacon");
     }
 }
