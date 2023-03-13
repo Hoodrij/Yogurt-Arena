4f2fb1ef2fed621ab5bc57783ae1f013f592ba3a
Slava Korzun <Hoodrij@gmail.com>
2023-03-13 19:49:54 +0200

    Simple Enemies movement

--- Yogurt-Arena-Unity/Assets/Scripts/Entities/Overmind/UpdateOvermindDestinationJob.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Overmind/UpdateOvermindDestinationJob.cs
@@ -1,4 +1,6 @@
-﻿namespace Yogurt.Arena
+﻿using UnityEngine;
+
+namespace Yogurt.Arena
 {
     public struct UpdateOvermindDestinationJob : IUpdateJob
     {
@@ -6,9 +8,16 @@
         {
             AgentAspect player = Query.Of<AgentAspect>().With<PlayerTag>().Single();
 
+            int agentIndex = 0;
+            
             foreach (AgentAspect agentAspect in Query.Of<AgentAspect>().Without<PlayerTag>())
             {
-                agentAspect.State.Destination = player.State.Position;
+                // agentAspect.State.Destination = player.State.Position;
+                
+                float moveRadius = 0.1f;
+                float time = Time.realtimeSinceStartup + agentIndex;
+                agentAspect.State.Destination += new Vector3(moveRadius * Mathf.Sin(time), 0, moveRadius * Mathf.Cos(time));
+                agentIndex++;
             }
         }
     }
