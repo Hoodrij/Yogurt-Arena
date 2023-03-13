4e693dd3b6fcc37ad1668083fd323edfa92f52c9
Slava Korzun <Hoodrij@gmail.com>
2023-03-13 19:22:24 +0200

    Extensions

---
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/Extensions/BoundsEx.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+namespace Yogurt.Arena
+{
+    public static class BoundsEx
+    {
+        public static Vector3 GetRandomPoint(this Bounds bounds, string seed = "random")
+        {
+            if (seed == "random")
+                seed = Time.realtimeSinceStartup.ToString();
+
+            Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+            float x = pseudoRandom.Next((int) (bounds.min.x * 100), (int) (bounds.max.x * 100)) / 100f;
+            float y = pseudoRandom.Next((int) (bounds.min.y * 100), (int) (bounds.max.y * 100)) / 100f;
+            float z = pseudoRandom.Next((int) (bounds.min.z * 100), (int) (bounds.max.z * 100)) / 100f;
+            return new Vector3(x, y, z);
+        }
+    }
+}
---
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/Extensions/FloatEx.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Yogurt.Arena
+{
+    public static class FloatEx
+    {
+        public static float RandomTo(this float f)
+        {
+            return Random.Range(0, f);
+        }
+    }
+}
