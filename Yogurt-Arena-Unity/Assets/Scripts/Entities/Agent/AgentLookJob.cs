0b20ab6d633b02afc5c6f6c7a75fbbdc86f46b36
Slava Korzun <Hoodrij@gmail.com>
2023-03-18 17:26:31 +0200

    Proper Look at Target

--- Yogurt-Arena-Unity/Assets/Scripts/Entities/Agent/AgentLookJob.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Agent/AgentLookJob.cs
@@ -10,16 +10,18 @@ namespace Yogurt.Arena
             {
                 // if (agent.State.Velocity.magnitude < 0.01f) continue;
 
+                BodyState body = agent.Body;
+                
                 if (agent.BattleState.Target.Exist)
                 {
                     BodyState targetBody = agent.BattleState.Target.Get<BodyState>();
-                    agent.Body.LookTarget = targetBody.Position;
+                    body.LookTarget = targetBody.Position;
                 }
                 else
                 {
-                    BodyState body = agent.Body;
                     body.LookTarget = body.Position + body.Velocity.WithY(0);
                 }
+                body.LookTarget.y = body.Position.y;
                 
                 agent.View.transform.DOLookAt(agent.Body.LookTarget, 0.3f);
             }
