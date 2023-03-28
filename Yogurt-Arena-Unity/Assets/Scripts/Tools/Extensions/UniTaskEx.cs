305afb7ed0fc33973f994b50ed3ea90448b80bbb
Slava Korzun <Hoodrij@gmail.com>
2023-03-28 17:52:04 +0300

    UniTaskEx

---
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/Extensions/UniTaskEx.cs
@@ -0,0 +1,13 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Yogurt.Arena
+{
+    public static class UniTaskEx
+    {
+        public static async UniTask<T> WhenAny<T>(UniTask<T> task1, UniTask<T> task2)
+        {
+            (int winArgumentIndex, T result1, T result2) = await UniTask.WhenAny(task1, task2);
+            return winArgumentIndex == 0 ? result1 : result2;
+        }
+    }
+}
