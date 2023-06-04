895db46505a919393fa462111ed44ec754ed9b29
Slava Korzun <Hoodrij@gmail.com>
2023-06-04 14:14:04 +0300

    WaitForEntityChanged observer job

---
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/WaitForEntityChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace Yogurt.Arena
+{
+    public struct WaitForEntityChanged
+    {
+        public async UniTask Run(Func<Entity> entityGetter)
+        {
+            Entity initialEntity = entityGetter.Invoke();
+
+            await UniTask.WaitWhile(() => initialEntity == entityGetter.Invoke());
+        }
+    }
+}
