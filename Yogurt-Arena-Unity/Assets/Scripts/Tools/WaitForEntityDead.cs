5dc69a27b6c2b5d75201f68b4ed2c8d2a163c0e4
Slava Korzun <Hoodrij@gmail.com>
2023-06-04 14:31:10 +0300

    WaitForEntityDead

---
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/WaitForEntityDead.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Yogurt.Arena
+{
+    public struct WaitForEntityDead
+    {
+        public async UniTask Run(Entity entity)
+        {
+            await UniTask.WaitWhile(() => entity.Exist);
+        }
+    }
+}
