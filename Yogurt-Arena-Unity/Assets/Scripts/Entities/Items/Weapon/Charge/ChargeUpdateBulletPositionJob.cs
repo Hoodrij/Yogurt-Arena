81789834f36bd75fb93cb4acb4ae3f7a6d22bda1
Slava Korzun <Hoodrij@gmail.com>
2023-06-05 12:43:42 +0300

    Cleanup

--- Yogurt-Arena-Unity/Assets/Scripts/Entities/Items/Weapon/Charge/ChargeUpdateBulletPositionJob.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Items/Weapon/Charge/ChargeUpdateBulletPositionJob.cs
@@ -1,5 +1,4 @@
-﻿using Cysharp.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Yogurt.Arena
 {
--- Yogurt-Arena-Unity/Assets/Scripts/Entities/Items/Weapon/Rain/UpdateRainTargetJob.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Items/Weapon/Rain/UpdateRainTargetJob.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using Cysharp.Threading.Tasks;
 
 namespace Yogurt.Arena
 {
