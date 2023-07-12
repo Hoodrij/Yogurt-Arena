704ce24d58b00cfc33825d8bdfb54ae3cf046300
Slava Korzun <Hoodrij@gmail.com>
2023-07-12 17:19:40 +0300

    Update Boot.cs

--- Yogurt-Arena-Unity/Assets/Scripts/Boot.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Boot.cs
@@ -9,24 +9,6 @@ namespace Yogurt.Arena
             Destroy(gameObject);
             await new GameFactoryJob().Run();
             await new RunWorldJob().Run();
-
-            123.log();
-            // Awaitable task = test();
-            // task.Cancel();
-            // await Wait.Any(test(), test2());
-            456.log();
-        }
-        
-        async Awaitable test2()
-        {
-            await Awaitable.WaitForSecondsAsync(2);
-            2.log();
-        }
-
-        async Awaitable test()
-        {
-            await Awaitable.WaitForSecondsAsync(1);
-            1.log();
         }
     }
 }
