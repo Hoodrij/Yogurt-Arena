25503f5d57e8cb07b7319f756fd58f32dd295572
Slava Korzun <Hoodrij@gmail.com>
2023-07-23 19:03:04 +0300

    kek Data separation

--- Yogurt-Arena-Unity/Assets/Scripts/Entities/Data/Data.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Data/Data.cs
@@ -14,14 +14,17 @@ namespace Yogurt.Arena
         public InputData Input;
         public CameraData Camera;
         public BeaconData Beacon;
+        
         [Header("Agents")]
         public AgentData Player;
         public AgentData ChargeEnemy;
         public OvermindData Overmind;
+        
         [Header("Weapon")]
         public RifleData Rifle;
         public RainData Rain;
         public ChargeData Charge;
+        
         [Header("Items")]
         public ItemSpotData ItemSpot;
         public HealingPotionData HealingPotion;
