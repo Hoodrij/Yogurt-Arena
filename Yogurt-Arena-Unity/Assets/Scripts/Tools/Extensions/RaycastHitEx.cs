a245b010f7001ac1729eb8a3b4d8836f67f87d1f
Slava Korzun <Hoodrij@gmail.com>
2023-08-10 20:10:53 +0300

    RaycastHitEx

---
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/Extensions/RaycastHitEx.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Yogurt.Arena
+{
+    public static class RaycastHitEx
+    {
+        public static Entity GetEntity(this RaycastHit hit)
+        {
+            EntityLink entityLink = hit.transform.GetComponentInParent<EntityLink>();
+            return entityLink != null ? entityLink.Entity : default;
+        }
+    }
+}
