5f9bed2640a619057ef1b7fab98d84ba6ac6f354
Slava Korzun <Hoodrij@gmail.com>
2023-08-11 20:56:30 +0300

    Wait with lifetime

--- Yogurt-Arena-Unity/Assets/Scripts/Tools/Wait/Wait.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/Wait/Wait.cs
@@ -1,38 +1,44 @@
 ﻿using System;
+using System.Threading;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Yogurt.Arena
 {
     public static class Wait
     {
+        static CancellationToken lifetime => Application.exitCancellationToken;
+        
         public static UniTask While(Func<bool> predicate)
         {
-            return UniTask.WaitWhile(predicate);
+            return UniTask.WaitWhile(predicate, cancellationToken: lifetime);
         }
         
         public static UniTask Until(Func<bool> predicate)
         {
-            return UniTask.WaitUntil(predicate);
+            return UniTask.WaitUntil(predicate, cancellationToken: lifetime);
         }
 
         public static UniTask Update()
         {
-            return UniTask.NextFrame();
+            return UniTask.NextFrame(cancellationToken: lifetime);
         }
 
         public static UniTask Seconds(float seconds)
         {
-            return UniTask.WaitForSeconds(seconds);
+            return UniTask.WaitForSeconds(seconds, cancellationToken: lifetime);
         }
 
         public static UniTask Any(params UniTask[] tasks)
         {
-            return UniTask.WhenAny(tasks);
+            return UniTask.WhenAny(tasks)
+                .AttachExternalCancellation(Application.exitCancellationToken);
         }
         
         public static UniTask All(params UniTask[] tasks)
         {
-            return UniTask.WhenAll(tasks);
+            return UniTask.WhenAll(tasks)
+                .AttachExternalCancellation(Application.exitCancellationToken);
         }
     }
 }
