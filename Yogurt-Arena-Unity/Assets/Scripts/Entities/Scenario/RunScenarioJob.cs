431a3cd52a7570bdd140d621eb5b23d66e655557
Slava Korzun <Hoodrij@gmail.com>
2023-08-17 14:44:11 +0300

    Update RunScenarioJob.cs

--- Yogurt-Arena-Unity/Assets/Scripts/Entities/Scenario/RunScenarioJob.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Scenario/RunScenarioJob.cs
@@ -6,13 +6,12 @@ namespace Yogurt.Arena
     {
         public async UniTask Run()
         {
-            // new RunOvermindBehaviorJob().Run();
             await new Quest.PickupWeaponQuest().Run();
-            await Wait.Seconds(1);
             await new LevelUpJob().Run();
             
-            // await new Quest.KillEnemiesQuest(3).Run();
-            // await new LevelUpJob().Run();
+            new RunOvermindBehaviorJob().Run();
+            await new Quest.KillEnemiesQuest(3).Run();
+            await new LevelUpJob().Run();
         }
     }
 }
