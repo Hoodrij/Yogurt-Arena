cccd622b50d00b25520e5a8df9371f554d1694d8
Slava Korzun <Hoodrij@gmail.com>
2023-08-20 23:05:51 +0300

    Update ObjectEx.cs

--- Yogurt-Arena-Unity/Assets/Scripts/Tools/Extensions/ObjectEx.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/Extensions/ObjectEx.cs
@@ -2,9 +2,9 @@
 {
     public static class ObjectEx
     {
-        public static T log<T>(this T t)
+        public static T log<T>(this T t, string prefix = "")
         {
-            UnityEngine.Debug.Log(t);
+            UnityEngine.Debug.Log(prefix + t);
             return t;
         }
     }
