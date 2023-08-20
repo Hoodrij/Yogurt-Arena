55ac66bbdc1c07a07ba40cbcd12b9389b072b318
Slava Korzun <Hoodrij@gmail.com>
2023-08-20 21:09:32 +0300

    GiveItemJob

---
+++ Yogurt-Arena-Unity/Assets/Scripts/Entities/Inventory/GiveItemJob.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Yogurt.Arena
+{
+    public struct GiveItemJob
+    {
+        public async UniTask Run(ItemType itemType, AgentAspect owner)
+        {
+            if (itemType == ItemType.Empty)
+                return;
+            
+            ItemAspect item = await new ItemFactoryJob().Run(itemType, owner);
+            UpdateWeapon();
+
+            item.Config.UseJob.Run(item);
+            return;
+
+
+            void UpdateWeapon()
+            {
+                if (!item.Config.Tags.HasFlag(ItemTags.Weapon)) 
+                    return;
+                
+                owner.Inventory.Weapon.Kill();
+                owner.Inventory.Weapon = item;
+            }
+        }
+    }
+}
