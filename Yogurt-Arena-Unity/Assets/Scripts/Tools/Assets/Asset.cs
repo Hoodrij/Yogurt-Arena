fc3ca37d2e654d14f83e11a8f4b6f705c32db2a3
Slava Korzun <Hoodrij@gmail.com>
2026-01-07 00:36:55 +0200

    Asset InstantiateAsync

--- Yogurt-Arena-Unity/Assets/Scripts/Tools/Assets/Asset.cs
+++ Yogurt-Arena-Unity/Assets/Scripts/Tools/Assets/Asset.cs
@@ -9,7 +9,9 @@ public class Asset<TComponent> : IAsset<TComponent> where TComponent : Component
 
     public async UniTask<TComponent> Spawn()
     {
-        TComponent result = Object.Instantiate(Prefab).GetComponent<TComponent>();
-        return result;
+        GameObject[] instantiateAsync = await Object.InstantiateAsync(Prefab);
+        return instantiateAsync[0].GetComponent<TComponent>();
+        // TComponent result = Object.Instantiate(Prefab).GetComponent<TComponent>();
+        // return result;
     }
 }
